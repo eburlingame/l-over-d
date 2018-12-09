860927dfb88663a8ff9f66da212b07e51662f634
Eric Burlingame <eric@burlingames.com>
2018-12-08 20:44:54 -0800

    Updated quiz

--- _site/public/js/study.js
+++ _site/public/js/study.js
@@ -7,21 +7,6 @@ const GAVEUP = 3;
 const SEQUENTIAL = 0;
 const RANDOM = 1;
 
-/**
- * Shuffles array in place.
- * @param {Array} a items An array containing the items.
- */
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
-
 QuizView = function(quizData, questionContainer, progressContainer)
 {
 	self = this;
